fccc5426a07fd4268b1079650da73a3fe99a1e93
Quentin Beudard <quentin.beudard.dev@gmail.com>
2019-02-17 17:22:24 +0100

    ajout commentaires

--- epidemic.py
+++ epidemic.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import numpy as np
 
+# Utilise la matrice d'adjacence pour trouver la transition
 def adjacency_to_transition( row ):
     degree=0
     for x in np.nditer(row):
@@ -8,11 +9,11 @@ def adjacency_to_transition( row ):
             degree+=1
     return np.divide(row, degree)
 
+
+# Utilise numpy pour trouver le vecteur propre à partir de la matrice de transition
 def page_rank(P):
-    # use numpy LAPACK solver
     eigenvalues, eigenvectors = np.linalg.eig(P.T)
     ind = eigenvalues.argsort()
-    # eigenvector of largest eigenvalue at ind[-1], normalized
     largest = np.array(eigenvectors[:, ind[-1]]).flatten().real
     norm = float(largest.sum())
     return dict(zip(G, map(float, largest / norm)))
@@ -21,19 +22,24 @@ def page_rank(P):
 
 # ======================================================
 
+# Chargement du graph
 G = nx.read_edgelist('facebook/0.edges', create_using=nx.DiGraph(),nodetype=int)
-print(G.edges())
 
+# Generation de la matrice d'adjacence
 adjacency_matrix = nx.adjacency_matrix(G, nodelist=sorted(G.nodes())).todense()
 print("ADJACENCY MATRIX : \n",adjacency_matrix)
 
+# Generation de la matrice de transition
 transition_matrix = np.apply_along_axis(adjacency_to_transition, axis=1, arr=adjacency_matrix)
+# on transpose pour qu'elle soit indéxé en colonne et non pas en ligne
 transition_matrix = transition_matrix.transpose()
 print ("\n\nTRANSITION MATRIX : \n",transition_matrix)
 
+# Algo Page Rank
 pagerank = page_rank(transition_matrix)
 print ("\n\n Page Rank : \n", pagerank)
 
+# Simulation Ici hihi
 
 
 
