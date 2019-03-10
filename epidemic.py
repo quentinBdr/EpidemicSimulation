cc88e72697927c0d687594359c8749fd8d7f89b5
Quentin Beudard <quentin.beudard.dev@gmail.com>
2019-03-10 19:08:06 +0100

    ajout du code pour matrice de transition

--- epidemic.py
+++ epidemic.py
@@ -6,14 +6,20 @@ from random import randint
 import matplotlib.pyplot as plt
 
 
-GRAPH_FILE_PATH = 'Wiki-Vote.txt'
+GRAPH_FILE_PATH = 'p2p.txt'
 
 def nb_infected(G):
     nb=0
-    for j in range(0, len(G)):
-        if G.has_node(j):
-            if int(G.node[j]['infected']) == 1:
-                nb += 1
+    for j in G.nodes():
+        if int(G.node[j]['infected']) == 1:
+            nb += 1
+    return nb
+
+def nb_vaccinated(G):
+    nb=0
+    for j in G.nodes():
+        if int(G.node[j]['infected']) == 2:
+            nb += 1
     return nb
 
 # Utilise la matrice d'adjacence pour trouver la transition
@@ -52,7 +58,8 @@ def create_infection_vector(G,x, mode):
 
     # ajout des vaccinés selon le résultat de pagerank dans le vecteur
     if mode == "pagerank":
-        pagerank = nx.pagerank(G, alpha=0.85)
+        print("Pagerank algo enabled")
+        pagerank = nx.pagerank(G, alpha=0.85, max_iter=100000000)
         for i in range(0, nb_to_be_vaccinated):
             maxPage = max(pagerank, key=pagerank.get)
             G.node[maxPage]['infected'] = 2
@@ -79,57 +86,73 @@ def create_infection_vector(G,x, mode):
 G = nx.read_edgelist(GRAPH_FILE_PATH, create_using=nx.DiGraph(),nodetype=int)
 print(nx.info(G))
 
+adjacency_matrix = nx.to_numpy_matrix(G)
+adjacency_matrix = nx.adjacency_matrix(G, nodelist=sorted(G.nodes())).todense()
+
+transition_matrix = np.apply_along_axis(adjacency_to_transition, axis=1, arr=adjacency_matrix)
+transition_matrix = transition_matrix.transpose()
+
+
 # Initialisation des paramètres de la simulation
 
-time = 140     # nombre d'itérations de la simulation
+time = 80     # nombre d'itérations de la simulation
 x = 0.05       # pourcentage d'individus aléatoirement infectés initialement = pourcentage d'individus vaccinés initialement
 v = 0.2        # probabilité à chaque itération d'un individu de transmettre l'infection à un voisin
 gamma = 0.24   # probabilité à chaque itération d'un individu de guérir de l'infection
 alpha = 0.8
-delta = 1 - alpha
+delta = 1 - alpha   # probabilité d'infecté un non-voisin
 
 G = create_infection_vector(G, x, "normal")
 
 nb_infect = []
 times = []
 
-len_G = len(G)
+len_G = len(G.nodes())
 print("INFECTION VECTOR : ", list(G.nodes(data=True)))
+
+random.seed(5342876)
+
 
 for i in range(0,time):
 
     nb = nb_infected(G)
+    nb_v = nb_vaccinated(G)
     times.append(i)
     nb_infect.append(nb)
 
     print('-------- ITERATION ',i, '--------')
     print("NB INFECTED : ", nb)
 
-    for j in range(0, len_G):
-        if G.has_node(j):
-            # Contamination des voisins
+    for j in G.nodes():
+
+        # Contamination des voisins
+        voisins = list(G.neighbors(j))
+        if len(voisins) > 0:
+            for voisin in voisins:
+                rand = np.random.uniform(0, 1)
+                if rand <= v and int(G.node[voisin]['infected']) != 2:
+                    G.node[voisin]['infected'] = 1
+
+
+        # ===============
+        # NOEUD INFECTE
+        if int(G.node[j]['infected']) == 1:
+
+            # CONTAMINER SES NON-VOISINS
             neighb = list(G.neighbors(j))
-            if len(neighb) > 0:
-                for k in list(neighb):
-                    rand = random.uniform(0, 1)
-                    if rand <= v and int(G.node[k]['infected']) == 0:
-                        G.node[k]['infected'] == 1
+            rand = np.random.uniform(0, 1) # proba d'infecter des non-voisins
+            non_neighb = list(set(G.nodes()) - set(neighb) - {j}) # non-voisins du noeud
+            if rand <= delta:
+                rand = randint(0, len(non_neighb) - 1)
+                if int(G.node[non_neighb[rand]]['infected']) != 2:
+                    G.node[non_neighb[rand]]['infected'] = 1
 
-            # ===============
-            # NOEUD INFECTE
-            if int(G.node[j]['infected']) == 1:
+    for j in G.nodes():
+        # SE GUERIR SOIS MEME
+        rand = np.random.uniform(0, 1)
+        if rand <= gamma:
+            G.node[j]['infected'] = 0
 
-                # Proba de guérir
-                rand = random.uniform(0, 1)
-                if rand <= gamma:
-                    G.node[j]['infected'] = 0
-
-                rand = random.uniform(0, 1) # proba d'infecter des non-voisins
-                non_neighb = list(set(G.nodes) - set(neighb) - {j}) # non-voisins du noeud
-                if rand <= delta:
-                    rand = randint(0, len(non_neighb) - 1)
-                    if G.node[non_neighb[rand]]['infected'] < 2:
-                        G.node[non_neighb[rand]]['infected'] = 1
 
 plt.plot(times, nb_infect)
 plt.show()
