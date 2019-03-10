c0005fdfcf196b669c3b462f0f36b7ad8c9fe2d7
Quentin Beudard <quentin.beudard.dev@gmail.com>
2019-03-10 16:15:24 +0100

    modif simulation

--- epidemic.py
+++ epidemic.py
@@ -37,7 +37,7 @@ def page_rank(P):
     norm = float(largest.sum())
     return dict(zip(G, map(float, largest / norm)))
 
-def create_infection_vector(G,x):
+def create_infection_vector(G,x, mode):
 
     # calcul du nombre d'infectés initial
     len_G = len(G)
@@ -51,13 +51,12 @@ def create_infection_vector(G,x):
     nx.set_node_attributes(G, 0, 'infected')
 
     # ajout des vaccinés selon le résultat de pagerank dans le vecteur
-    a=0
-    pagerank = nx.pagerank(G, alpha=0.85)
-    for i in range(0, nb_to_be_vaccinated):
-        maxPage = max(pagerank, key=pagerank.get)
-        G.node[maxPage]['infected'] = 2
-        pagerank.pop(maxPage)
-        a=i
+    if mode == "pagerank":
+        pagerank = nx.pagerank(G, alpha=0.85)
+        for i in range(0, nb_to_be_vaccinated):
+            maxPage = max(pagerank, key=pagerank.get)
+            G.node[maxPage]['infected'] = 2
+            pagerank.pop(maxPage)
 
     i=0
     while i < nb_to_be_infected:
@@ -72,41 +71,27 @@ def create_infection_vector(G,x):
         i += 1
     return G
 
-# ======================================================
+# ==================================================================================================
+# ==================================================================================================
+# ==================================================================================================
 
 # Chargement du graphe
 G = nx.read_edgelist(GRAPH_FILE_PATH, create_using=nx.DiGraph(),nodetype=int)
 print(nx.info(G))
-
-# Generation de la matrice d'adjacence
-#adjacency_matrix = nx.adjacency_matrix(G, nodelist=sorted(G.nodes())).todense()
-#print("ADJACENCY MATRIX : \n",adjacency_matrix)
-
-# Generation de la matrice de transition
-#transition_matrix = np.apply_along_axis(adjacency_to_transition, axis=1, arr=adjacency_matrix)
-# on transpose pour qu'elle soit indéxé en colonne et non pas en ligne
-#transition_matrix = transition_matrix.transpose()
-#print("\n\nTRANSITION MATRIX : \n",transition_matrix)
-
-# Algo Page Rank
-#pagerank = page_rank(transition_matrix)
-#print("\n\n Page Rank : \n", pagerank)
-
-# Simulation Ici hihi
 
 # Initialisation des paramètres de la simulation
 
 time = 140     # nombre d'itérations de la simulation
 x = 0.05       # pourcentage d'individus aléatoirement infectés initialement = pourcentage d'individus vaccinés initialement
 v = 0.2        # probabilité à chaque itération d'un individu de transmettre l'infection à un voisin
-gamma = 0.15   # probabilité à chaque itération d'un individu de guérir de l'infection
+gamma = 0.24   # probabilité à chaque itération d'un individu de guérir de l'infection
 alpha = 0.8
 delta = 1 - alpha
 
-G = create_infection_vector(G,x)
+G = create_infection_vector(G, x, "normal")
 
 nb_infect = []
-t = []
+times = []
 
 len_G = len(G)
 print("INFECTION VECTOR : ", list(G.nodes(data=True)))
@@ -114,7 +99,7 @@ print("INFECTION VECTOR : ", list(G.nodes(data=True)))
 for i in range(0,time):
 
     nb = nb_infected(G)
-    t.append(i)
+    times.append(i)
     nb_infect.append(nb)
 
     print('-------- ITERATION ',i, '--------')
@@ -122,19 +107,22 @@ for i in range(0,time):
 
     for j in range(0, len_G):
         if G.has_node(j):
-            if int(G.node[j]['infected']) == 1:
-                rand = random.uniform(0, 1)
-                if rand <= gamma: # probabilité de guérir
-                    G.node[j]['infected'] = 0
+            # Contamination des voisins
+            neighb = list(G.neighbors(j))
+            if len(neighb) > 0:
+                for k in list(neighb):
+                    rand = random.uniform(0, 1)
+                    if rand <= v and int(G.node[k]['infected']) == 0:
+                        G.node[k]['infected'] == 1
 
-                neighb = list(G.neighbors(j))
-                #print("NEIGHBORS OF ", j, ":", neighb)
-                if len(neighb) > 0:
-                    for k in list(neighb):
-                        rand = random.uniform(0, 1)
-                        if rand <= v and int(G.node[k]['infected']) < 1: # probabilité d'infecter un voisin sain non vacciné
-                            #print("    NODE ",k," HAS BEEN INFECTED")
-                            G.node[k]['infected'] == 1
+            # ===============
+            # NOEUD INFECTE
+            if int(G.node[j]['infected']) == 1:
+
+                # Proba de guérir
+                rand = random.uniform(0, 1)
+                if rand <= gamma:
+                    G.node[j]['infected'] = 0
 
                 rand = random.uniform(0, 1) # proba d'infecter des non-voisins
                 non_neighb = list(set(G.nodes) - set(neighb) - {j}) # non-voisins du noeud
@@ -142,8 +130,7 @@ for i in range(0,time):
                     rand = randint(0, len(non_neighb) - 1)
                     if G.node[non_neighb[rand]]['infected'] < 2:
                         G.node[non_neighb[rand]]['infected'] = 1
-                    #print("NON VOISIN ", non_neighb[rand], " INFECTE")
 
-plt.plot(t, nb_infect)
+plt.plot(times, nb_infect)
 plt.show()
 
--- pagerank.py
+++ pagerank.py
@@ -19,3 +19,23 @@ def page_rank_power(A, iter):
         x0 = x0 / np.linalg.norm(x0, 1)
     return x0
 
+
+# Generation de la matrice d'adjacence
+#adjacency_matrix = nx.adjacency_matrix(G, nodelist=sorted(G.nodes())).todense()
+#print("ADJACENCY MATRIX : \n",adjacency_matrix)
+
+# Generation de la matrice de transition
+#transition_matrix = np.apply_along_axis(adjacency_to_transition, axis=1, arr=adjacency_matrix)
+# on transpose pour qu'elle soit indéxé en colonne et non pas en ligne
+#transition_matrix = transition_matrix.transpose()
+#print("\n\nTRANSITION MATRIX : \n",transition_matrix)
+
+# Algo Page Rank
+#pagerank = page_rank(transition_matrix)
+#print("\n\n Page Rank : \n", pagerank)
+non_neighb = list(set(G.nodes) - set(neighb) - {j}) # non-voisins du noeud
+                if rand <= delta:
+                    rand = randint(0, len(non_neighb) - 1)
+                    if G.node[non_neighb[rand]]['infected'] < 2:
+                        G.node[non_neighb[rand]]['infected'] = 1
+                    #print("NON VOISIN ", non_neighb[rand], " INFECTE")
